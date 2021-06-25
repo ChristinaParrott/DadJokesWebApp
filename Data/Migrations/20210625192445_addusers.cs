5b949eb0aab72d46567d2a193af861abdcf0c5cd
ChristinaParrott <chipps73@gmail.com>
2021-06-25 16:54:07 -0400

    Add project files.

---
+++ Data/Migrations/20210625192445_addusers.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DadJokesWebApp.Data.Migrations
+{
+    public partial class addusers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "JokeUser",
+                table: "Joke",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JokeUser",
+                table: "Joke");
+        }
+    }
+}
---
+++ Models/Joke.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DadJokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        [DisplayName("Joke Question")]
+        public string JokeQuestion { get; set; }
+        [DisplayName("Joke Answer")]
+        public string JokeAnswer { get; set; }
+        [DisplayName("Joke User")]
+        public string JokeUser { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
